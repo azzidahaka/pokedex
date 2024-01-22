c2db2a36b94011daca146db48af3440368ea543d
Azzidahaka <Azzidahaka@gmail.com>
2024-01-22 13:06:28 -0700

    commit - Created pokemon array and populated  with 3  pokemon class

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favoriteFood = 'noodles';
-document.write(favoriteFood);
+//Empty array for pokemons
+let pokemonList = [];
+//Populate pokemonList array with some pokemons
+pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] },
+    { name: "Squirtle", height: 0.5, types: ["water"] }
+];
