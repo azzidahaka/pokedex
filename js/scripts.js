49d78851c72b5dc6193985deab1afdaffe8cf5ac
Azzidahaka <Azzidahaka@gmail.com>
2024-01-22 21:16:53 -0700

    Updated pokemon list to be created and populated

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-//Empty array for pokemons
-let pokemonList = [];
-//Populate pokemonList array with some pokemons
-pokemonList = [
+//Created and populated pokemonList
+const pokemonList = [
     { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
     { name: "Charmander", height: 0.6, types: ["fire"] },
     { name: "Squirtle", height: 0.5, types: ["water"] }
-];
+  ];
