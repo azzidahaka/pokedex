af679c4b3be7af72af2adc53e0c203f45ec2f46f
Azzidahaka <Azzidahaka@gmail.com>
2024-01-22 21:30:10 -0700

    created a loop to go through pokemonList and print out the name, also react if the pokemon is igger than average

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ const pokemonList = [
     { name: "Charmander", height: 0.6, types: ["fire"] },
     { name: "Squirtle", height: 0.5, types: ["water"] }
   ];
+
+//Loop through pokemonList and print out the name and height
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>'+'The ' + pokemonList[i].name + ' is ' + pokemonList[i].height + 'm tall.');
+    //Comment on size of pokemon ff height is > 0.6
+    if (pokemonList[i].height>0.6){
+        document.write('-- Wow, that\’s big!');
+    }
+
+}
