9bba2025731c003412938aec0d35fc5687d8be0b
Azzidahaka <Azzidahaka@gmail.com>
2024-01-25 20:43:23 -0700

    created iffe to avoid accidentally accessing pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
-//Created and populated pokemonList
-const pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Charmander", height: 0.6, types: ["fire"] },
-    { name: "Squirtle", height: 0.5, types: ["water"] }
-  ];
+//IIFE to avoide accessing global state
+let pokemonRepository = (function () {
+    //Created and populated pokemonList
+    let pokemonList = [
+        { name: "Bulbasaur", height: 0.7, types: ["grass", "poison","gas"] },
+        { name: "Charmander", height: 0.6, types: ["fire"] },
+        { name: "Squirtle", height: 0.5, types: ["water"] }
+    ];
 
-//Loop through pokemonList and print out the name and height
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>'+'The ' + pokemonList[i].name + ' is ' + pokemonList[i].height + 'm tall.');
-    //Comment on size of pokemon ff height is > 0.6
-    if (pokemonList[i].height>0.6){
-        document.write('-- Wow, that\’s big!');
-    }
-    document.write("</p>");
-
-}
+    return{
+        getAll, add, addV
+    };
+})();
